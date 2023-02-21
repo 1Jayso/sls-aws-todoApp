f517170095332daa05b43b701fd6a7a94f1799de
SJA <adodey1@yahoo.com>
2023-02-21 23:05:23 +0000

    first commit

---
+++ client/src/config.ts
@@ -0,0 +1,12 @@
+
+const apiId = 'mpwurg4muc'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+
+  domain: 'dev-3ag6nabvb85ne8fo.us.auth0.com',
+  clientId: 'CEs6asFRk9mxd47XdCTzRuVW5tQL3eMg', 
+  callbackUrl: 'http://localhost:3000/callback'
+
+}
+
